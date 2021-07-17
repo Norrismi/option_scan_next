b9d402d14e5aea162090aa2cbc6de633dec66a2b
Norrismi <Norrismi0@gmail.com>
2021-07-17 13:44:04 -0400

    first commit

---
+++ components/Navbar.js
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="logo">
+        <h1>Option Scan</h1>
+      </div>
+      <Link href="/"><a>Home</a></Link>
+      <Link href="/about"><a>About</a></Link>
+  
+    </nav>
+);
+}
+ 
+export default Navbar;
